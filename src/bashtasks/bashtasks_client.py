e433ce29f2c0e5f63eb5b37a5a7f41714e9ee3b0
javierarilos <javier.arilos@gmail.com>
2015-10-23 15:01:42 +0200

    implement: bashtasks_client.execute_task

--- src/bashtasks/bashtasks_client.py
+++ src/bashtasks/bashtasks_client.py
@@ -2,6 +2,8 @@
 """
 import json
 import time
+from datetime import datetime
+
 from constants import TASK_RESPONSES_POOL, TASK_POOL
 from bashtasks.rabbit_util import connect_and_declare
 
@@ -13,6 +15,10 @@ def currtimemillis():
 
 
 def post_task(command, reply_to=TASK_RESPONSES_POOL):
+    """ posts command to executors via RabbitMQ TASK_POOL
+        does NOT wait for response.
+        :return: <dict> message created for the task.
+    """
     print '>>>>> posting task ', command
     msg = {
         'command': command,
@@ -22,6 +28,26 @@ def post_task(command, reply_to=TASK_RESPONSES_POOL):
     }
     msg_str = json.dumps(msg)
     channel_inst.basic_publish(exchange=TASK_POOL, routing_key='', body=msg_str)
+    return msg
+
+
+def execute_task(command, reply_to=TASK_RESPONSES_POOL, timeout=10):
+    """ posts command to executors via RabbitMQ TASK_POOL
+        synchronously waits for response.
+        :return: <dict> response message.
+    """
+    task = post_task(command, reply_to)
+    start_waiting = datetime.now()
+    while True:
+        method_frame, header_frame, body = channel_inst.basic_get(TASK_RESPONSES_POOL)
+        if body:
+            channel_inst.basic_ack(method_frame.delivery_tag)
+            return json.loads(body)
+        else:
+            if (datetime.now() - start_waiting).total_seconds() > timeout:
+                raise Exception('Timeout ({}secs) waiting for response 2 msg: {} in queue: "{}"'
+                                .format(timeout, task['correlation_id'], reply_to))
+            time.sleep(0.01)
 
 
 class BashTasks:
@@ -38,10 +64,12 @@ def init(host='127.0.0.1', usr='guest', pas='guest', channel=None):
 
     bashtasks = BashTasks()
     bashtasks.post_task = post_task
+    bashtasks.execute_task = execute_task
     return bashtasks
 
 
 def reset():
+
     global channel_inst
     if channel_inst is not None:
         channel_inst.close()
--- src/test/test_bashtasks.py
+++ src/test/test_bashtasks.py
@@ -1,11 +1,13 @@
 import unittest
 import time
 from datetime import datetime
+import multiprocessing
 
 import bashtasks as bashtasks_mod
 import bashtasks.rabbit_util as rabbit_util
-from bashtasks.constants import TASK_POOL
+from bashtasks.constants import TASK_POOL, TASK_RESPONSES_POOL
 from pika_assertions import assertMessageInQueue
+import executor
 
 unavailable_rabbit = not rabbit_util.is_rabbit_available()
 
@@ -29,9 +31,9 @@ class TestBashTasks(unittest.TestCase):
 
 
 @unittest.skipIf(unavailable_rabbit, "SKIP integration Tests: rabbitmq NOT available")
-class IntegrationTesting(unittest.TestCase):
+class IntegTestPostTask(unittest.TestCase):
     def setUp(self):
-        pass
+        rabbit_util.purge()
 
     def tearDown(self):
         bashtasks_mod.reset()
@@ -42,6 +44,59 @@ class IntegrationTesting(unittest.TestCase):
 
         queue_name = TASK_POOL
         body = assertMessageInQueue(queue_name)
+
+    def test_post_task_creates_correct_task_msg(self):
+        ls_task = ['ls', '-la']
+        bashtasks = bashtasks_mod.init()
+        msg = bashtasks.post_task(ls_task)
+
+        self.assertEqual(msg['command'], ls_task)
+        self.assertEqual(msg['reply_to'], TASK_RESPONSES_POOL)
+        self.assertTrue('correlation_id' in msg)
+        self.assertTrue('request_ts' in msg)
+
+
+def start_executor_process():
+    p = multiprocessing.Process(target=executor.start_executor)
+    p.start()
+    time.sleep(0.1)
+    return p
+
+
+def kill_executor_process(p):
+    p.terminate()
+
+
+@unittest.skipIf(unavailable_rabbit, "SKIP integration Tests: rabbitmq NOT available")
+class IntegTestExecuteTask(unittest.TestCase):
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        bashtasks_mod.reset()
+
+    def test_execute_task_raises_on_timeout(self):
+        ls_task = ['ls', '-la']
+        bashtasks = bashtasks_mod.init()
+        with self.assertRaises(Exception):
+            response_msg = bashtasks.execute_task(ls_task, timeout=0.3)
+
+    def test_execute_task_returns_correct_response_msg(self):
+        try:
+            p = start_executor_process()
+
+            bashtasks = bashtasks_mod.init()
+            ls_task = ['ls', '-la']
+            response_msg = bashtasks.execute_task(ls_task)
+
+            self.assertTrue('returncode' in response_msg)
+            self.assertTrue('request_ts' in response_msg)
+            self.assertEqual(response_msg['command'], ls_task)
+            self.assertEqual(response_msg['reply_to'], TASK_RESPONSES_POOL)
+            self.assertTrue('correlation_id' in response_msg)
+        finally:
+            kill_executor_process(p)
+            time.sleep(0.2)
 
 
 if __name__ == '__main__':
