e56591a45ec47dcf7460ff35775763d108131946
javierarilos <javier.arilos@gmail.com>
2015-11-09 16:38:18 +0100

    add simplest setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='bashtasks',
+      version='0.2',
+      description='execute bash tasks remotely',
+      author='Javier Arias',
+      author_email='javier.arilos@gmail.com',
+      url='https://github.com/javierarilos/bashtasks.git',
+      packages=['bashtasks']
+)
