6f578e31d4ee6e0cc7c332623ad34866533b98f2
javierarilos <javier.arilos@gmail.com>
2016-01-27 13:42:07 +0100

    make logger PY2 compatible

--- src/bashtasks/logger.py
+++ src/bashtasks/logger.py
@@ -1,15 +1,23 @@
 import logging
 
+def hasHandlers(logger):
+    """returns True if logger has handlers.
+       py2 and py3 compatible implementation
+    """
+    if logger.__dict__.get('hasHandlers', None):
+        return logger.hasHandlers()
+    else:
+        return len(logger.handlers) > 0
 
 def get_logger(name=None):
     name = name or __name__
     logger = logging.getLogger(name)
-    if logger.hasHandlers():
+    if hasHandlers(logger):
         return logger
 
     logger.setLevel(logging.DEBUG)
-    formatter = logging.Formatter('{asctime};{name};{threadName};{levelname};{message}',
-                                  style='{', datefmt='%Y-%m-%d;%H:%M:%S')
+    formatter = logging.Formatter('%(asctime)s;%(name)s;%(threadName)s;%(levelname)s;%(message)s',
+                                  datefmt='%Y-%m-%d;%H:%M:%S')
     ch = logging.StreamHandler()
     ch.setFormatter(formatter)
     ch.setLevel(logging.DEBUG)
